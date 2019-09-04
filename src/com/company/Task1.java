a4e36076642b04715469e87250de0c062e70e172
Medigi27 <pmezenina@gmail.com>
2019-09-04 15:55:53 +0300

    Initial commit

---
+++ src/com/company/Task1.java
@@ -0,0 +1,31 @@
+package com.company;
+import java.io.*;
+import java.util.*;
+
+
+
+
+public class Task1 {
+    public static void main(String[] args) throws Exception {
+        FileReader fr = new FileReader( "INPUT.TXT");
+        Scanner scan = new Scanner(fr);
+
+        String test;
+
+
+
+
+
+        fr.close();
+
+
+
+
+    }
+    public static void writer  {
+        // здесь ваш код
+        FileWriter fw = new FileWriter( "OUTPUT.TXT");
+        fw.close();
+    }
+
+}
