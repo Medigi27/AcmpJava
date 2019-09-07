0dc4ac582b63aa6b049bfb389731379084e5ad11
Medigi27 <pmezenina@gmail.com>
2019-09-07 19:38:55 +0300

    version7

--- src/com/Task6/Task6.java
+++ src/com/Task6/Task6.java
@@ -1,4 +1,4 @@
-package com.Task5;
+package com.Task6;
 
 import java.io.File;
 import java.io.IOException;
@@ -6,23 +6,19 @@ import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Task5 {
+public class Task6 {
 
     public static void main(String[] argv) throws IOException {
         Scanner sc = new Scanner(new File("input.txt"));
-        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+        int S = sc.nextInt();
         sc.close();
-        int y = a * b;
+        int x = S / 6;
+        int Kat = x * 4;
         PrintWriter pw = new PrintWriter(new File("output.txt"));
-        if (y == c) {
-
-            pw.println("y");
-        }
-        else {
-
-            pw.println("no");
-        }
+        pw.println(x + " " + Kat + " " + x);
         pw.close();
+
+
 
     }
 }
---
+++ src/com/Task7/Task7.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task6 {
+
+    public static void main(String[] argv) throws IOException {
+
+    }
+}
