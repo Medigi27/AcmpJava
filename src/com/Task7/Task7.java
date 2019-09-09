9dd0348fde9f9f6715acf3eedae0adfc8096a35f
Medigi27 <pmezenina@gmail.com>
2019-09-09 13:53:52 +0300

    version8

--- src/com/Task7/Task7.java
+++ src/com/Task7/Task7.java
@@ -4,9 +4,27 @@ import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Task6 {
+public class Task7 {
 
     public static void main(String[] argv) throws IOException {
+        Scanner sc = new Scanner(new File("input.txt"));
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+        int[] tab = {a, b, c};
+        int max = Arrays.stream(tab).max().getAsInt();
+        PrintWriter pw = new PrintWriter(new File("output.txt"));
+        if (a > 727 || a < 94 ) {
+            pw.println("Error");
+        }
+        if (b > 727 || b < 94 ) {
+            pw.println("Error");
+        }
+        if (c > 727 || c < 94 ) {
+            pw.println("Error");
+        }
+        else {
+            pw.print(max);
+        }
+        pw.close();
 
     }
 }
